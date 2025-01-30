ee4837d62f38ad807f6cc0cef2ceac794cbbdcfe
Mett <164408568+chocomett@users.noreply.github.com>
2025-01-31 06:54:51 +0700

    Up/CG0002/Upload File

---
+++ libs.py
@@ -0,0 +1,5 @@
+def wellcome_message(message):
+    style = "*" * (len(message) + 6)
+    print(style)
+    print(f"** {message} **")
+    print(style)
--- main.py
+++ main.py
@@ -1,36 +1,38 @@
 import random # import libary dari bawaan pythom
+from libs import wellcome_message
 
-wellcome_message = "WELLCOME TO CHOCOMETT GAMES!"
-hadiah_utama = random.randint(1, 4) # random integer dari angka 1 samapi dengan 4
+wellcome_message("WELLCOME TO CHOCOMETT GAMES!")
 
-print(wellcome_message)
 nama_user = input("Masukan Nama Anda... ")
-print(f"""Heyy, {nama_user}. Apakah kamu siap bermain tebak tebakan bersamaku ?
-""")
+while nama_user == "":
+    nama_user = input("isi dulu nama lu egeee... ")
 
-jawaban_pembuka = input("Masukan Jawaban Anda, Y/N... ")
-
-if jawaban_pembuka.upper() == "Y": # .upper() agar output dari input user di konversi menjadi uppercase
-    print(f"Lesgowww mari kita bermain")
-else:
-    print(f"Sayang sekali, Mari kita berjumpa dilain waktu")
-    exit()
+while True:
+    bentuk_goa = "|_|"
+    goa = [bentuk_goa] * 4 #jadikan array agar dapet diambil data didalamnya dan dapat dirubah
+    goa_hadiah = goa.copy() #membuat agar goa pada hasil akhir dapat mengikuti desain yang sama seperti goa awal namun isi goa hadia tidak diketahui dari awal
+    hadiah_utama = random.randint(1, 4) # random integer dari angka 1 samapi dengan 4
+    goa_hadiah[hadiah_utama - 1] = "|🏆|" #memunculkan hadiah dengan mereplace sesuai dengan random randint lalu - 1 agar hasil sesuai dengan bahasa manusia
+    new_goa = " ".join(goa) #menghilangkan tanda koma dan bukatutup kurung di awal
+    goa_hadiah = " ".join(goa_hadiah) #menghilangkan tanda koma dan bukatutup kurung di hasil akhir
     
-print(f'''
-Halo {nama_user}! Coba perhatikan 4 Kolom dibawah ini
+    print(f"\nHalo {nama_user}! Coba perhatikan 4 Kolom dibawah ini\n\n{new_goa}")
+    print("\nCoba Tebak kolom nomer berapakah yang terdapat Hadiah Utama.\nMari Kita Coba Peruntunganmu!!!\n")
 
-|_| |_| |_| |_|
-''')
-print("""
-Coba Tebak kolom nomer berapakah yang terdapat Hadiah Utama.
-Mari Kita Coba Peruntunganmu!!!
-""")
+    jawaban_user = int(input("Masukan Nomer Peruntunganmu [ 1 / 2 / 3 / 4 ]... ")) # int() agar tipe data input menjadi Integer
+    while jawaban_user != 1 and jawaban_user != 2 and jawaban_user != 3 and jawaban_user != 4: #!= tidak sama dengan | and : dan
+        jawaban_user = int(input("CUMAN BISA 4 ANGKA INI KOCAK [ 1 / 2 / 3 / 4 ]... "))
 
-jawaban_user = int(input("Masukan Nomer Peruntunganmu 1/2/3/4... ")) # int() agar tipe data input menjadi Integer
-
-print(f"Pilihanmu kolom nomer {jawaban_user}")
-
-if jawaban_user == hadiah_utama:
-    print(f"Wow Jawaban kamu benar, Hadiah Utama terdapat dikolom nomer {hadiah_utama}")
-else:
-    print(f"Sayang banget, Hadiah Utama terdapat pada nomer {hadiah_utama}")
+    if jawaban_user == hadiah_utama:
+        print(f"\n{goa_hadiah}")
+        print(f"\nWow Jawaban kamu benar, Hadiah Utama terdapat dikolom nomer {hadiah_utama}\n")
+        print("SELAMAT KAMU MENANG 🏆🏆 \n")
+    else:
+        print(f"\n{goa_hadiah}")
+        print(f"\nSayang banget, Hadiah Utama terdapat pada nomer {hadiah_utama}\n")
+        print("UBUR UBUR IKAN LELE COBA LAGI LEEE 😁😁 \n")
+    
+    main_lagi = input("apakah ingin bermain lagi [Y / N]... ")
+    if main_lagi.upper() == "N":
+        print("\nProgram Berhernti, Terimakasih Sudah Bermain Chocomett Games 🤩🤩\n")
+        break
